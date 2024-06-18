2c05ac2b3e8ec3edb4ae1412c35c4df45d6e5604
LucasBGarcia <barbozagarcia@yahoo.com.br>
2024-06-18 13:59:28 -0300

    configura react-router-dom

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,1 +1,10 @@
-export { };
+import { Route, Routes, Navigate } from "react-router-dom"
+
+export const AppRoutes = () => {
+    return (
+        <Routes>
+            <Route path="/" element={<p>aqui</p>} />
+            <Route path="*" element={<Navigate to={"/"} />} />
+        </Routes>
+    )
+}
